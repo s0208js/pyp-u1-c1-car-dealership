7170bd6d973a2f9715b3c724b452a04437a77d1b
s0208js <s0208js@hotmail.com>
2018-07-21 01:58:08 -0400

    john sek assigment

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,29 @@
 class Contract(object):
-    pass
+    def __init__(self, vehicle, customer, monthly_payments=0,length_in_months=0):
+      self.vehicle=vehicle
+      self.customer=customer
+      self.monthly_payments=monthly_payments
+      self.length_in_months=length_in_months
+      
+    def monthly_value(self):
+      return self.total_value()\
+             /(self.monthly_payments if self.monthly_payments>0 else self.length_in_months)
 
+      
+      
 
 class BuyContract(Contract):
-    def __init__(self, vehicle, customer, monthly_payments):
-        pass
+  #sale_price() + (1.07 * monthly_payments * sale_price() / 100) - (discount if employee)
+    def total_value(self):
+      return (self.vehicle.sale_price()+\
+             (self.vehicle.i*self.monthly_payments*self.vehicle.sale_price()/100))\
+             *(0.9 if self.customer.is_employee() else 1)
 
 
 class LeaseContract(Contract):
-    def __init__(self, vehicle, customer, length_in_months):
-        pass
+  #sale_price() + (sale_price() * 1.2 / length_in_months)
+    def total_value(self):
+      return (self.vehicle.sale_price()+\
+             (self.vehicle.l/self.length_in_months*self.vehicle.sale_price()))*\
+             (0.9 if self.customer.is_employee() else 1)
+
--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,41 @@
 class Vehicle(object):
-    def __init__(self, maker, model, year, base_price, miles):
-        pass
+  s=1.2
+  p=0.004
+  i=1.07
+  l=1.2
+  def __init__(self, maker, model, year, base_price, miles):
+    self.maker = maker
+    self.model=model
+    self.year=year
+    self.base_price=base_price
+    self.miles=miles
+     
+  def sale_price(self):
+    return self.base_price*self.s
+  
+  def purchase_price(self):
+    return self.sale_price()-(self.miles*self.p)
 
 
 class Car(Vehicle):
-    pass
+  s=1.2
+  p=0.004
+  i=1.07
+  l=1.2
+  
 
 
 class Motorcycle(Vehicle):
-    pass
+  s=1.1
+  p=0.009
+  i=1.03
+  l=1
+  
 
 
 class Truck(Vehicle):
-    pass
+  s=1.6
+  p=0.02
+  i=1.11
+  l=1.7
+  
